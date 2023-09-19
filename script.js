20278ed9bdc30b3a957b6ff1d5e3858d9d608e17
Estefania Gauto <ingestefaniagauto@gmail.com>
2023-09-18 21:09:39 -0300

    Primer commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('modooscuro').onclick= function(){
+    document.body.style.backgroundColor='black'
+    document.getElementById('modooscuro').style.color= 'white'
+    document.getElementsByClassName('text-custom').style.color= 'white'
+
+}
